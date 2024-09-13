a512b0ca923e6e7bf2bf1f61344de37efa26dc29
momo199000 <mohib.ahmed79@myhunter.cuny.edu>
2024-09-13 16:37:40 -0400

    Initial commit

---
+++ learn-react-1/src/main.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'
+
+
+function StudentInfo(props) {
+  return (
+    <div>
+      <div>
+        {props.lastName}, {props.firstName}
+      </div>
+      <ul>
+        <li>
+          <strong>ID:</strong> {props.sId}
+        </li>
+        <li>
+          <strong>School:</strong> {props.school}
+        </li>
+        <li>
+          <strong>Major:</strong> {props.major}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function ClassList() {
+  return (
+    <div>
+      <h1>Welcome to CTP</h1>
+      <p>List of Students</p>
+      {studentList.map((student) => (
+        <StudentInfo {...student} key={student.sId} />
+      ))}
+    </div>
+  );
+}
+
+function CountApp() {
+  const [numClicks, setNumClicks] = useState(0);
+
+  const handleClick = (event) => {
+    setNumClicks(numClicks + 1);
+  };
+
+  return (
+    <div>
+      <p>The button has been clicked {numClicks} times</p>
+      <button onClick={handleClick}>Click this button</button>
+    </div>
+  );
+}
+const studentList = [
+  {
+    firstName: "Misty",
+    lastName: "Knight",
+    sId: "234",
+    school: "Queens College",
+    major: "Law",
+  },
+  {
+    firstName: "Jessica",
+    lastName: "Jones",
+    sId: "434",
+    school: "Brooklyn College",
+    major: "CS",
+  },
+  {
+    firstName: "Colleen",
+    lastName: "Wing",
+    sId: "233",
+    school: "Queens College",
+    major: "CS",
+  },
+  {
+    firstName: "Dare",
+    lastName: "Devil",
+    sId: "876",
+    school: "CCNY",
+    major: "Law",
+  },
+  {
+    firstName: "Luke",
+    lastName: "Cage",
+    sId: "323",
+    school: "CCNY",
+    major: "Math",
+  },
+]
+const htmlContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(htmlContainer);
+root.render(<CountApp />);
+
